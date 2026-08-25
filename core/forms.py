0e4a8c60876603f3ee954283f503c56535fe534a
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-25 23:29:15 +0530

    added prescription form

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Prescription
 
 class LoginForm(forms.Form):
     username=forms.CharField()
@@ -8,3 +9,8 @@ class RegisterForm(forms.Form):
     username=forms.CharField()
     email=forms.EmailField()
     password=forms.CharField(widget=forms.PasswordInput())
+
+class PrescriptionForm(forms.ModelForm):
+    class Meta:
+        model=Prescription
+        fields=['age', 'condition', 'prior_medical_history']
