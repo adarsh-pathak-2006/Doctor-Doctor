200e2ae6534daf59c35908a9ce0b7d5aa7ebad59
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-25 23:15:47 +0530

    added forms in the core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Prescription(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    age=models.PositiveIntegerField()
+    condition=models.TextField()
+    prior_medical_history=models.TextField()
+    drug_prescription=models.TextField()
+    analysis=models.TextField()
+    created_on=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.user.username
+
