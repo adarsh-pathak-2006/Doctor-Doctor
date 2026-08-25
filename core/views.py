ba9edce550fefe98e90654545961c7ffb48617a6
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-25 23:22:59 +0530

    fixed import issue

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 class RegisterView(View):
     def get(self, request):
